6571c752f059b90128d3a30a69f2b3657104da23
vkuleski <kuleski.viktor@gmail.com>
2024-10-31 20:17:00 +0100

    pr-verify (#3)
    * add dotnet test to pr-verify.yml
    
    * failing test

--- src/GitHubActionsDotNet.Api.Tests/Models/WeatherForecastTests.cs
+++ src/GitHubActionsDotNet.Api.Tests/Models/WeatherForecastTests.cs
@@ -7,11 +7,13 @@ public class WeatherForecastTests
     [Fact]
     public void TemperatureFShouldReturnCorrectValueBasedOnTemperatureC()
     {
+
         var weatherForecast = new WeatherForecast
         {
             TemperatureC = 0
         };
 
-        weatherForecast.TemperatureF.Should().Be(32);
+        const int expectedTempC = 32;
+        weatherForecast.TemperatureF.Should().Be(expectedTempC);
     }
 }
